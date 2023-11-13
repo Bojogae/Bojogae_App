2bf9fa05a7c13144ce0580828161f9456d66b536
wndudwkd003 <ymail3@naver.com>
2023-11-13 22:41:39 +0900

    init

---
+++ app/src/main/java/com/wnzyan/face_detect_distance_opencv_uvc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wnzyan.face_detect_distance_opencv_uvc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
