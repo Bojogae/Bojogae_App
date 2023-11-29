461fde64c999d7a9a9c65d7031212a9b115ee895
wooseong <emforhs5346@naver.com>
2023-11-29 22:39:48 +0900

    init

---
+++ opencv/build/intermediates/cxx/Debug/1g1o70m4/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\User\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\User\\StudioProjects\\c\\opencv\\.cxx\\Debug\\1g1o70m4\\x86" ^
+  opencv_jni_shared
