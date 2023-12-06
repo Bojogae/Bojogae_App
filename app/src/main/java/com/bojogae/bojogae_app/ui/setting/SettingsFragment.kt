22192ed33cc06e058f84697ff84823c330690bc6
wndudwkd003 <ymail3@naver.com>
2023-12-07 00:41:11 +0900

    환경설정 디자인 수정 완료

--- app/src/main/java/com/bojogae/bojogae_app/ui/setting/SettingsFragment.kt
+++ app/src/main/java/com/bojogae/bojogae_app/ui/setting/SettingsFragment.kt
@@ -5,16 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.SpinnerAdapter
 import com.bojogae.bojogae_app.R
+import com.bojogae.bojogae_app.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
+    private var _viewBinding: FragmentSettingsBinding? = null
+    private val viewBinding get() = _viewBinding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+    ): View {
+        _viewBinding = FragmentSettingsBinding.inflate(layoutInflater)
 
-        return view
+
+        val devicesArray = resources.getStringArray(R.array.string_array_devices)
+        val spinnerAdapter = ArrayAdapter(requireContext(), R.layout.cv_spinner_black_bg_white_text, devicesArray)
+        viewBinding.spinnerDiscoverDevice.adapter = spinnerAdapter
+        spinnerAdapter.setDropDownViewResource(R.layout.cv_spinner_black_bg_white_text)
+
+
+
+
+        return viewBinding.root
     }
 }
